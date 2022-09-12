c18fbc343674e746a35e0333a7477cf42fed2a0a
Dsakkos <dmaster456@yahoo.com>
2022-09-11 23:06:12 -0400

    First React With JSX
    First React With JSX Assignment

---
+++ first_jsx/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="my-class">Hello Dojo!</h1>
+				<ul className="App-list">
+					<h2>Things I Need To Do:</h2>
+					<li className="App-list-item">Learn React</li>
+					<li className="App-list-item">Climb Mt. Everest</li>
+					<li className="App-list-item">Run A Marathon</li>
+					<li className="App-list-item">Feed The Dogs!!</li>
+				</ul>
+			</header>
+		</div>
+	);
+}
+
+export default App;
